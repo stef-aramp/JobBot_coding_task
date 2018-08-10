5772dd2267b71b5304b6d613c3442b9053f7caf0
stef-aramp <johndoe@example.com>
2018-08-10 20:45:22 +0300

    fix typo

--- part1/gettingPhilosophy.py
+++ part1/gettingPhilosophy.py
@@ -10,64 +10,65 @@ import requests
 from bs4 import BeautifulSoup
 #import urllib.request
 import re
-'''
-I used these links as test cases. 
-https://en.wikipedia.org/wiki/Special:Random worked fine as well
 
-url =  "https://en.wikipedia.org/wiki/Science" 
-url = "https://en.wikipedia.org/wiki/Natural_science"
-url = "https://en.wikipedia.org/wiki/International_law"
-url = "https://en.wikipedia.org/wiki/Politics"
-url = "https://en.wikipedia.org/wiki/Tokyo"
-'''
 
 url = "https://en.wikipedia.org/wiki/Special:Random"
-##################################
-number_of_searches = 50
-s = 0
-while s < number_of_searches:
-    def popTags(text):
-        # parentheses
-        text = re.sub(r'\(from .+\)|\(<a .+\)', '', str(text))
-        # sup tags
-        text = re.sub('<sup(.+?)/sup>', '',str(text))
-        return(text.strip())
+
+#################
+
+def popTags(text):
+    # parentheses
+    text = re.sub(r'\(from(.+?)\)|\(<.(.+?)\)', '', str(text))
+    # sup tags
+    text = re.sub('<sup(.+?)/sup>', '',str(text))
+    # sup tags
+    text = re.sub('<small(.+?)/small>', '',str(text))
+    return(text.strip())
     
-    def tillPhilosophy(url):
+def tillPhilosophy(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
         
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
+    #title = soup.find(id="firstHeading").text
         
-        #title = soup.find(id="firstHeading").text
-        
-        results = soup.find_all('div', {'class' : 'mw-parser-output'})
+    results = soup.find_all('div', {'class' : 'mw-parser-output'})
     
-        paragraph = results[0].find_all('p')[0]
-        
-        # if paragraph <p></p> is empty, go to the next p
+    paragraph = results[0].find_all('p')[0:2]
+    
+    # if paragraph <p></p> is empty, go to the next p
+    if paragraph[0].find('a') is None :
+        paragraph = paragraph[1]
         while paragraph.find('a') is None:
             paragraph = paragraph.find_next_sibling('p')
-            
-        if paragraph.span is not None:
-            paragraph.span.clear()    
-        else:
-            pass
+    else:
+        paragraph = paragraph[0]
+    
+    # clear some span
+    if paragraph.span is not None:
+        paragraph.span.clear()    
+    else:
+        pass
         
-        # get the text of paragraph
-        p_text = paragraph.get_text
+    # get the text of paragraph
+    p_text = paragraph.get_text
         
-        # remove noise links from tables, foot/hat/notes etc. & recreate BS object
-        new_par = BeautifulSoup(popTags(p_text))
-        return('https://en.wikipedia.org' + new_par.find('a')['href'], soup.find(id="firstHeading").text)
+    # remove noise links from tables, foot/hat/notes etc. & recreate BS object
+    new_par = BeautifulSoup(popTags(p_text),'lxml')
+    return('https://en.wikipedia.org' + new_par.find('a')['href'], soup.find(id="firstHeading").text)
     
-    # =============================================================================
-    #url = "https://en.wikipedia.org/wiki/Special:Random"
+# =============================================================================
     
-    next_link, title = tillPhilosophy("https://en.wikipedia.org/wiki/Fact")
-    
-    while title != 'Philosophy':
-        next_link, title = tillPhilosophy(url)
-        print(title)
-        url = next_link
+next_link, title = tillPhilosophy(url)
+
+number_of_searches = 50
+s = 0    
+  
+while title != 'Philosophy' and s < number_of_searches:
+    print(title)
+    next_link, title = tillPhilosophy(url)
+    url = next_link
     
     s +=1
+    if s == number_of_searches:
+        print('\n','Infinite Loop ∞')
+        
